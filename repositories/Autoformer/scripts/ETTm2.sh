b7ff7e1fb602e55d1e0af4d368983f1a4816c0fa
KwangryeolPark <pkr7098@gmail.com>
2025-06-11 13:49:46 +0900

    git_sync

---
+++ repositories/Autoformer/scripts/ETTm2.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+if [ -d "./venv" ]; then
+    source ./venv/bin/activate
+fi
+
+if [ ! -d "./logs" ]; then
+    mkdir ./logs
+fi
+
+if [ ! -d "./logs/LTSF" ]; then
+    mkdir ./logs/LTSF
+fi
+
+seq_len=96
+model_name=Autoformer
+
+root_path_name=./dataset/ETT-small/
+data_path_name=ETTh1.csv
+model_id_name=ETTh1
+data_name=ETTh1
+
+random_seed=2021
+
+for pred_len in 24 48 96 288 672
+do
+    python -u run.py \
+      --random_seed $random_seed \
+      --is_training 1 \
+      --root_path $root_path_name \
+      --data_path $data_path_name \
+      --model_id $model_id_name'_'$seq_len'_'$pred_len \
+      --model $model_name \
+      --data $data_name \
+      --features M \
+      --seq_len 96 \
+      --label_len 48 \
+      --pred_len $pred_len \
+      --e_layers 2 \
+      --d_layers 1 \
+      --factor 1 \
+      --enc_in 7 \
+      --dec_in 7 \
+      --c_out 7 \
+      --des 'Exp' \
+      --freq 't' \
+      --itr 1 \
+      > >(tee logs/LTSF/${model_name}_${model_id_name}_${seq_len}_${pred_len}.log)  # This will log to both console and file
+done
